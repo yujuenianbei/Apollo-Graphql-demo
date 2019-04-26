d19820fbf6e949c32da86bc0f01611fd39f6f701
yujuenianbei <365913672@qq.com>
2019-04-26 18:52:53 +0800

    ver 0.1

--- src/mutationSimple.js
+++ src/mutationSimple.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import gql from "graphql-tag";
 import { Mutation } from "react-apollo";
 
@@ -9,32 +10,33 @@ const ADD_TODO = gql`
     }
   }
 `;
+let input;
+class AddTodo extends Component {
+    render() {
+        return (
+            <Mutation mutation={ADD_TODO}>
+                {(addTodo, { data }) => (
+                    <div>
+                        <form
+                            onSubmit={e => {
+                                e.preventDefault();
+                                addTodo({ variables: { type: input.value } });
+                                input.value = "";
+                            }}
+                        >
+                            <input
+                                ref={node => {
+                                    input = node;
+                                }}
+                            />
+                            <button type="submit">Add Todo</button>
+                        </form>
+                    </div>
+                )}
+            </Mutation>
+        );
+    }
 
-const AddTodo = () => {
-  let input;
-
-  return (
-    <Mutation mutation={ADD_TODO}>
-      {(addTodo, { data }) => (
-        <div>
-          <form
-            onSubmit={e => {
-              e.preventDefault();
-              addTodo({ variables: { type: input.value } });
-              input.value = "";
-            }}
-          >
-            <input
-              ref={node => {
-                input = node;
-              }}
-            />
-            <button type="submit">Add Todo</button>
-          </form>
-        </div>
-      )}
-    </Mutation>
-  );
 };
 
 export default AddTodo
